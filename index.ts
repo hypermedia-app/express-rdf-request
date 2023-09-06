7c456aae7cd3265166959e86dbdd3a321e1f019b
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2023-09-06 14:03:43 +0200

    chore: update to ESM

--- index.ts
+++ index.ts
@@ -1,12 +1,13 @@
 import { DataFactory, DatasetCoreFactory, NamedNode } from 'rdf-js'
 import express from 'express'
-import clownface, { AnyPointer, GraphPointer } from 'clownface'
-import $rdf from 'rdf-ext'
-import * as rdfHandler from '@rdfjs/express-handler'
+import type { AnyPointer, GraphPointer } from 'clownface'
+import rdfHandler from '@rdfjs/express-handler'
 import asyncMiddleware from 'middleware-async'
-import * as absoluteUrl from 'absolute-url'
+import absoluteUrl from 'absolute-url'
 import isRelativeUrl from 'is-relative-url'
 import once from 'once'
+import type { Environment } from '@rdfjs/environment/Environment'
+import type ClownfaceFactory from 'clownface/Factory'
 
 declare module 'express-serve-static-core' {
   export interface Request {
@@ -23,11 +24,13 @@ declare module 'rdf-js' {
   interface Stream extends AsyncIterable<import('rdf-js').Quad> {}
 }
 
+type Env = Environment<DataFactory | DatasetCoreFactory | ClownfaceFactory>
+
 interface GetTerm {
-  (req: express.Request): NamedNode
+  ($rdf: Env, req: express.Request): NamedNode
 }
 
-function defaultTerm(req: express.Request) {
+function defaultTerm($rdf: Env, req: express.Request) {
   absoluteUrl.attach(req)
 
   return $rdf.namedNode(req.absoluteUrl())
@@ -35,25 +38,25 @@ function defaultTerm(req: express.Request) {
 
 interface Options {
   getTerm?: GetTerm
-  factory?: DataFactory & DatasetCoreFactory
+  factory: Env
 }
 
-export async function attach(req: express.Request, res: express.Response, { getTerm = defaultTerm, factory = $rdf }: Options = {}): Promise<void> {
-  await (rdfHandler as any).attach(req, res, {
+export async function attach(req: express.Request, res: express.Response, { getTerm = defaultTerm, factory }: Options): Promise<void> {
+  await rdfHandler.attach(req, res, {
     baseIriFromRequest: true,
     factory,
   })
 
   if (!req.resource) {
     req.resource = once(async () => {
-      const term = getTerm(req)
+      const term = getTerm(factory, req)
       if (!term) {
         throw new Error('Could not determine request term.')
       }
 
       const dataset = factory.dataset()
 
-      for (const quad of await req.dataset()) {
+      for (const quad of await req.dataset!()) {
         const { predicate, graph } = quad
         const subject = quad.subject.termType === 'NamedNode' && isRelativeUrl(quad.subject.value) ? term : quad.subject
         const object = quad.object.termType === 'NamedNode' && isRelativeUrl(quad.object.value) ? term : quad.object
@@ -61,14 +64,14 @@ export async function attach(req: express.Request, res: express.Response, { getT
         dataset.add(factory.quad(subject, predicate, object, graph))
       }
 
-      return clownface({ dataset }).node(term)
+      return factory.clownface({ dataset }).node(term)
     })
 
     res.resource = (pointer: AnyPointer) => res.dataset(pointer.dataset)
   }
 }
 
-export const resource = (options?: Options): express.RequestHandler => asyncMiddleware(async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+export const resource = (options: Options): express.RequestHandler => asyncMiddleware(async (req: express.Request, res: express.Response, next: express.NextFunction) => {
   await attach(req, res, options)
 
   next()
